d5f6b55ad5eb7431ea38311687f68c2928852c02
fse-dev <elmanoukdesign@gmail.com>
2019-02-19 01:34:47 -0500

    thicker directories

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ except:
 '''
 pseudo code testing...
 '''
+
 import requests
 from bs4 import BeautifulSoup
 from multiprocessing import Process
@@ -19,6 +20,16 @@ from multiprocessing import Process
 ENDPOINTS = ["https://finance.yahoo.com/quote/","https://www.investopedia.com/articles/investing/053116/10-largest-holdings-sp-500-aaplamznfb.asp"]     
 
 
+def createDirectory(path):
+    try:
+        os.mkdir(path)
+    except OSError:
+        print("Creation of the directory %s failed" % path)
+    else:
+        print("Successfully created the directory %s " % path)
+
+
+
 #gets the top 10 company thickers      
 def getCompanyName():
     result = requests.get(ENDPOINTS[1])
@@ -26,8 +37,10 @@ def getCompanyName():
     rawThickers = rawHtml.select('tr > td > p > a')
     thickers = []
     for i in range(0,len(rawThickers)): 
-        thickers.append(rawThickers[i].text)    
+        thickers.append(rawThickers[i].text)  
+        createDirectory(rawThickers[i].text)
     return(thickers) 
+
 
 #creates new urllist        
 def appendNames(urlList=[]):
@@ -38,8 +51,10 @@ def appendNames(urlList=[]):
     
 def getContent(domain):
     data = requests.get(domain)
-    return(data.status_code)
-        
+    return(data.status_code)      
+
+
+
 
 if __name__=='__main__':
     content_list = appendNames()
