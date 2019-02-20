a862e6d25c0fc595fb3865298dcc0a01e9575bdb
fse-dev <elmanoukdesign@gmail.com>
2019-02-19 22:18:21 -0500

    creating the html files in company directories

--- src/main.py
+++ src/main.py
@@ -13,11 +13,11 @@ pseudo code testing...
 
 import requests
 from bs4 import BeautifulSoup
-from multiprocessing import Process
 
 
+ENDPOINTS = ["https://finance.yahoo.com/quote/",
+             "https://www.investopedia.com/articles/investing/053116/10-largest-holdings-sp-500-aaplamznfb.asp"]
 
-ENDPOINTS = ["https://finance.yahoo.com/quote/","https://www.investopedia.com/articles/investing/053116/10-largest-holdings-sp-500-aaplamznfb.asp"]     
 
 
 def createDirectory(path):
@@ -27,9 +27,7 @@ def createDirectory(path):
         print("Creation of the directory %s failed" % path)
     else:
         print("Successfully created the directory %s " % path)
-
-
-
+    
 #gets the top 10 company thickers      
 def getCompanyName():
     result = requests.get(ENDPOINTS[1])
@@ -37,8 +35,11 @@ def getCompanyName():
     rawThickers = rawHtml.select('tr > td > p > a')
     thickers = []
     for i in range(0,len(rawThickers)): 
-        thickers.append(rawThickers[i].text)  
-        createDirectory(rawThickers[i].text)
+        company = rawThickers[i].text
+        filename = company+".html"
+        thickers.append(company)  
+        createDirectory(company)
+        open(company+"/"+filename, 'w')
     return(thickers) 
 
 
@@ -56,15 +57,10 @@ def getContent(domain):
 
 
 
+
 if __name__=='__main__':
     content_list = appendNames()
-    x = 0
-    for i in content_list:
-        x += 1
-        print('process', x)
-        p = Process(target=getContent, args=(i, ))
-        p.start()
-        p.join()
+    
      
   
    
