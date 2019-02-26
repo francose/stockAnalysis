e6374c66a6e1fb974bf2e227f41e3baf9f9648fc
fse-dev <elmanoukdesign@gmail.com>
2019-02-26 12:16:44 -0500

    preprocessing undone getTable method.

--- src/companyNames.py
+++ src/companyNames.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
+path = "Companies/"
 class CompanyNames(object):
 
     def __init__(self, url, thickers=[], urlList=[], contentList=[]):
@@ -31,20 +31,31 @@ class CompanyNames(object):
             res = requests.get(url)
             data = res.content
             raw = BeautifulSoup(data, 'html.parser')
-            tableHeader = raw.select('table > thead >tr >th')
-        self.getTableHeaders(tableHeader)
-        
-    def getTableHeaders(self, tableHeader, path="Companies/"):
-        headers = [i.text for i in tableHeader]
-        for i in self.thickers:
-            with open(path + i + "/" + i + ".csv", "w+") as f:
-                f.writelines(str(headers))
-        return(headers)
-    
-    def createDirectory(self, company="Companies/"):
+            tableBody = [ td.findAll('span') for td in raw.findAll(attrs={'data-test': 'historical-prices'})]
+        self.getTableBody(tableBody)
+
+    def getTableBody(self, tableBody, history=[]):
+        table = [ history.append(table.text) for table in tableBody[0]]
+        for row in range(0, len(table[7:697])):
+            obj= {
+                'date': history[row],
+                'open': history[row],
+                'high': history[row],
+                'low': history[row],
+                'close': history[row],
+                'adj_close': history[row],
+                'vol': history[row],
+            }
+            print(obj)
+
+        # for i in self.thickers:
+        #     with open(path + i + "/" + i + ".csv", "w+") as f:
+        #         f.writelines(str(tbody))
+            
+    def createDirectory(self):
         for dr in self.thickers:
             try:
-                abspath = company + dr
+                abspath = path + dr
                 os.mkdir(abspath)
                 open(abspath + "/" + dr + ".csv", "w+")
             except OSError:
